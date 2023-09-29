509ce3b1ab6e1ee1d03cb3f087c35e4ab2b263ee
dat.truong <dat.truong@secomm.vn>
2023-09-29 10:57:53 +0700

    add intercom script

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,11 +24,8 @@ const Home: NextPage = () => {
           __html: `
           window.intercomSettings = {
             api_base: "https://api-iam.intercom.io",
-            app_id: "aj4jwz60",
-            name: trangnguyen, // Full name
-            email: trang.thinguyen@theaccessgroup.com, // Email address
-            created_at: user.createdAt // Signup date as a Unix timestamp
-            }
+            app_id: "vh68ugh7"
+          };
           `,
         }}
       />
@@ -36,8 +33,9 @@ const Home: NextPage = () => {
       <script
         dangerouslySetInnerHTML={{
           __html: `
-              (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/aj4jwz60';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-            `,
+          // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/vh68ugh7'
+          (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/vh68ugh7';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+          `,
         }}
       />
     </div>
